5a887afef3a8817660bf97f3f3950cb591a2ab83
Jotony Guerra <jotonyguerra@gmail.com>
2022-08-02 21:24:41 -0700

    sendkeys and click testing

--- Automation/web/sessions.py
+++ Automation/web/sessions.py
@@ -11,11 +11,31 @@ try:
     wait = WebDriverWait(driver, 10)
     driver.get('https://the-internet.herokuapp.com')
     #driver.find_element(By.LINK_TEXT, 'Form Authentication')
-    wait.until(EC.presence_of_element_located(
+    form_auth_link = wait.until(EC.presence_of_element_located(
         (By.LINK_TEXT, 'Form Authentication')
     ))
-    wait.until(EC.url_to_be('https://the-internet.herokuapp.com/'))
+    form_auth_link.click()
 
+    username = wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#username')
+    ))
+    
+    username.send_keys('tomsmith')
+    
+    password = driver.find_element(By.CSS_SELECTOR, '#password')
+    password.send_keys('SuperSecretPassword!')
+    
+    driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
+    
+    wait.until(EC.presence_of_element_located(
+        (By.LINK_TEXT, 'Logout')
+    )).click()
+
+    flash = wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '#flash')
+    ))
+    
+    assert 'logged out' in flash.text 
     
 finally:
     driver.quit()
