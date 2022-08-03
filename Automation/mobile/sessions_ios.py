bfa928232a966fca8fd3b52a8f02d6c88f5115a7
Jotony Guerra <jotonyguerra@gmail.com>
2022-08-02 22:21:07 -0700

    running and simulating appium ios

--- Automation/mobile/sessions_ios.py
+++ Automation/mobile/sessions_ios.py
@@ -1,2 +1,23 @@
 from appium import webdriver
+from os import path 
 
+#installing appium, and ios compatible software
+
+CUR_DIR = path.dirname(path.abspath(__file__))
+APP = path.join(CUR_DIR, 'TheApp.app.zip')
+APPIUM = 'https://localhost:4723' #change port
+
+CAPS = {
+    'platformName': 'iOS',
+    'platformVersion': '13.6', #may need to change
+    'deviceName': 'iPhone 11',
+    'automationName': 'XCUITest',
+    'app': APP 
+}
+
+
+driver = webdriver.Remote(
+    command_executor=APPIUM,
+    desired_capabilities=CAPS
+)
+driver.quit()
